595d72395624cd007da2f72648f2a40110dd72c6
imasaru <evanmr4@gmail.com>
2018-01-10 20:37:36 -0600

    Added Chinese Union Version (Traditional) Bible
    @VitalikL Can I remove the extra chapters that I added in order to please the scraper?

---
+++ bibles/zh-hant/cuv/info.js
@@ -0,0 +1,469 @@
+var info = {
+	"lang": "zh-hant",
+	"version": "cuv",
+	"books": [
+		{
+			"name": "創世記",
+			"numChapters": 50,
+			"synonyms": [
+				"創世記"
+			]
+		},
+		{
+			"name": "出埃及記",
+			"numChapters": 40,
+			"synonyms": [
+				"出埃及記"
+			]
+		},
+		{
+			"name": "利未記",
+			"numChapters": 27,
+			"synonyms": [
+				"利未記"
+			]
+		},
+		{
+			"name": "民數記",
+			"numChapters": 36,
+			"synonyms": [
+				"民數記"
+			]
+		},
+		{
+			"name": "申命記",
+			"numChapters": 34,
+			"synonyms": [
+				"申命記"
+			]
+		},
+		{
+			"name": "約書亞記",
+			"numChapters": 24,
+			"synonyms": [
+				"約書亞記"
+			]
+		},
+		{
+			"name": "士師記",
+			"numChapters": 21,
+			"synonyms": [
+				"士師記"
+			]
+		},
+		{
+			"name": "路得記",
+			"numChapters": 4,
+			"synonyms": [
+				"路得記"
+			]
+		},
+		{
+			"name": "撒母耳記上",
+			"numChapters": 31,
+			"synonyms": [
+				"撒母耳記上"
+			]
+		},
+		{
+			"name": "撒母耳記下",
+			"numChapters": 24,
+			"synonyms": [
+				"撒母耳記下"
+			]
+		},
+		{
+			"name": "列王紀上",
+			"numChapters": 22,
+			"synonyms": [
+				"列王紀上"
+			]
+		},
+		{
+			"name": "列王紀下",
+			"numChapters": 25,
+			"synonyms": [
+				"列王紀下"
+			]
+		},
+		{
+			"name": "歷代志上",
+			"numChapters": 29,
+			"synonyms": [
+				"歷代志上"
+			]
+		},
+		{
+			"name": "歷代志下",
+			"numChapters": 36,
+			"synonyms": [
+				"歷代志下"
+			]
+		},
+		{
+			"name": "以斯拉記",
+			"numChapters": 10,
+			"synonyms": [
+				"以斯拉記"
+			]
+		},
+		{
+			"name": "尼希米記",
+			"numChapters": 13,
+			"synonyms": [
+				"尼希米記"
+			]
+		},
+		{
+			"name": "以斯帖記",
+			"numChapters": 10,
+			"synonyms": [
+				"以斯帖記"
+			]
+		},
+		{
+			"name": "約伯記",
+			"numChapters": 42,
+			"synonyms": [
+				"約伯記"
+			]
+		},
+		{
+			"name": "詩篇",
+			"numChapters": 151,
+			"synonyms": [
+				"詩篇"
+			]
+		},
+		{
+			"name": "箴言",
+			"numChapters": 31,
+			"synonyms": [
+				"箴言"
+			]
+		},
+		{
+			"name": "傳道書",
+			"numChapters": 12,
+			"synonyms": [
+				"傳道書"
+			]
+		},
+		{
+			"name": "雅歌",
+			"numChapters": 8,
+			"synonyms": [
+				"雅歌"
+			]
+		},
+		{
+			"name": "以賽亞",
+			"numChapters": 67,
+			"synonyms": [
+				"以賽亞"
+			]
+		},
+		{
+			"name": "耶利米 書",
+			"numChapters": 53,
+			"synonyms": [
+				"耶利米 書"
+			]
+		},
+		{
+			"name": "耶利米 哀歌",
+			"numChapters": 5,
+			"synonyms": [
+				"耶利米 哀歌"
+			]
+		},
+		{
+			"name": "以西結書",
+			"numChapters": 49,
+			"synonyms": [
+				"以西結書"
+			]
+		},
+		{
+			"name": "但以理書",
+			"numChapters": 12,
+			"synonyms": [
+				"但以理書"
+			]
+		},
+		{
+			"name": "何西阿書",
+			"numChapters": 14,
+			"synonyms": [
+				"何西阿書"
+			]
+		},
+		{
+			"name": "約珥書",
+			"numChapters": 3,
+			"synonyms": [
+				"約珥書"
+			]
+		},
+		{
+			"name": "阿摩司書",
+			"numChapters": 9,
+			"synonyms": [
+				"阿摩司書"
+			]
+		},
+		{
+			"name": "俄巴底亞書",
+			"numChapters": 1,
+			"synonyms": [
+				"俄巴底亞書"
+			]
+		},
+		{
+			"name": "約拿",
+			"numChapters": 4,
+			"synonyms": [
+				"約拿"
+			]
+		},
+		{
+			"name": "彌迦書",
+			"numChapters": 7,
+			"synonyms": [
+				"彌迦書"
+			]
+		},
+		{
+			"name": "那鴻書",
+			"numChapters": 3,
+			"synonyms": [
+				"那鴻書"
+			]
+		},
+		{
+			"name": "哈巴谷書",
+			"numChapters": 3,
+			"synonyms": [
+				"哈巴谷書"
+			]
+		},
+		{
+			"name": "西番雅書",
+			"numChapters": 3,
+			"synonyms": [
+				"西番雅書"
+			]
+		},
+		{
+			"name": "哈該書",
+			"numChapters": 2,
+			"synonyms": [
+				"哈該書"
+			]
+		},
+		{
+			"name": "撒都該 迦利亞書",
+			"numChapters": 14,
+			"synonyms": [
+				"撒都該 迦利亞書"
+			]
+		},
+		{
+			"name": "瑪拉基書",
+			"numChapters": 4,
+			"synonyms": [
+				"瑪拉基書"
+			]
+		},
+		{
+			"name": "馬太 福音",
+			"numChapters": 28,
+			"synonyms": [
+				"馬太 福音"
+			]
+		},
+		{
+			"name": "馬可福音",
+			"numChapters": 16,
+			"synonyms": [
+				"馬可福音"
+			]
+		},
+		{
+			"name": "路加福音",
+			"numChapters": 24,
+			"synonyms": [
+				"路加福音"
+			]
+		},
+		{
+			"name": "約翰福音",
+			"numChapters": 21,
+			"synonyms": [
+				"約翰福音"
+			]
+		},
+		{
+			"name": "使徒行傳",
+			"numChapters": 28,
+			"synonyms": [
+				"使徒行傳"
+			]
+		},
+		{
+			"name": "羅馬書",
+			"numChapters": 16,
+			"synonyms": [
+				"羅馬書"
+			]
+		},
+		{
+			"name": "哥林多前書",
+			"numChapters": 16,
+			"synonyms": [
+				"哥林多前書"
+			]
+		},
+		{
+			"name": "哥林多後書",
+			"numChapters": 13,
+			"synonyms": [
+				"哥林多後書"
+			]
+		},
+		{
+			"name": "加拉太書",
+			"numChapters": 6,
+			"synonyms": [
+				"加拉太書"
+			]
+		},
+		{
+			"name": "以弗所書",
+			"numChapters": 6,
+			"synonyms": [
+				"以弗所書"
+			]
+		},
+		{
+			"name": "腓立比書",
+			"numChapters": 4,
+			"synonyms": [
+				"腓立比書"
+			]
+		},
+		{
+			"name": "歌羅西書",
+			"numChapters": 4,
+			"synonyms": [
+				"歌羅西書"
+			]
+		},
+		{
+			"name": "帖撒羅尼迦前書",
+			"numChapters": 5,
+			"synonyms": [
+				"帖撒羅尼迦前書"
+			]
+		},
+		{
+			"name": "帖撒羅尼迦後書",
+			"numChapters": 3,
+			"synonyms": [
+				"帖撒羅尼迦後書"
+			]
+		},
+		{
+			"name": "提摩太前書",
+			"numChapters": 6,
+			"synonyms": [
+				"提摩太前書"
+			]
+		},
+		{
+			"name": "提摩太後書",
+			"numChapters": 4,
+			"synonyms": [
+				"提摩太後書"
+			]
+		},
+		{
+			"name": "提多書",
+			"numChapters": 3,
+			"synonyms": [
+				"提多書"
+			]
+		},
+		{
+			"name": "腓利門書",
+			"numChapters": 1,
+			"synonyms": [
+				"腓利門書"
+			]
+		},
+		{
+			"name": "希伯來書",
+			"numChapters": 13,
+			"synonyms": [
+				"希伯來書"
+			]
+		},
+		{
+			"name": "雅各",
+			"numChapters": 5,
+			"synonyms": [
+				"雅各"
+			]
+		},
+		{
+			"name": "彼得 前書",
+			"numChapters": 5,
+			"synonyms": [
+				"彼得 前書"
+			]
+		},
+		{
+			"name": "彼得後書",
+			"numChapters": 3,
+			"synonyms": [
+				"彼得後書"
+			]
+		},
+		{
+			"name": "約翰壹書",
+			"numChapters": 5,
+			"synonyms": [
+				"約翰壹書"
+			]
+		},
+		{
+			"name": "約翰貳書",
+			"numChapters": 1,
+			"synonyms": [
+				"約翰貳書"
+			]
+		},
+		{
+			"name": "約翰參書",
+			"numChapters": 1,
+			"synonyms": [
+				"約翰參書"
+			]
+		},
+		{
+			"name": "猶大書",
+			"numChapters": 1,
+			"synonyms": [
+				"猶大書"
+			]
+		},
+		{
+			"name": "啟示錄",
+			"numChapters": 22,
+			"synonyms": [
+				"啟示錄"
+			]
+		}
+	]
+};
+module.exports = info;
